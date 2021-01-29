33b5257ef43f49a923b78ba5c595a32a2ab3649e
Michelle <michelle.daya@ucdenver.edu>
2021-01-29 11:46:21 -0700

    Fixed bug where multi-allelic variants caused the ref and alt alleles to be incorrectly constructed

--- create_mr_mega_input.R
+++ create_mr_mega_input.R
@@ -1,4 +1,5 @@
 library(SeqArray)
+library(stringr)
 
 args <- commandArgs(trailingOnly = TRUE)
 out.file.name <- args[1]
@@ -19,10 +20,20 @@ for (i in 1:length(rdata.files)) {
   gds.file <- gds.files[i]
   gds <- seqOpen(gds.file)
   
+  #Get required variables from gds file
   variant.ids <- seqGetData(gds, "variant.id")
   chromosome <- seqGetData(gds, "chromosome")
   position <- seqGetData(gds, "position")
   alleles <- gsub(",", ":", seqGetData(gds, "allele"))
+
+  #Get rid of multi-allelic variants; retaining them would screw up the ref and alt allele assignment
+  nr.alleles <- str_count(alleles, ":") + 1
+  variant.ids <- variant.ids[nr.alleles == 2]
+  chromosome <- chromosome[nr.alleles == 2]
+  position <- position[nr.alleles == 2]
+  alleles <- alleles[nr.alleles == 2]
+
+  #Construct variables according to the required format
   ref.alleles <- unlist(strsplit(alleles, split=":"))[seq(1,length(alleles)*2,2)]
   alt.alleles <- unlist(strsplit(alleles, split=":"))[seq(2,length(alleles)*2,2)]
   snp.ids <- paste(chromosome, position, alleles, sep=":")
@@ -31,9 +42,11 @@ for (i in 1:length(rdata.files)) {
                           alt.allele=alt.alleles,
                           snp.id=snp.ids)
   seqClose(gds)
- 
+
+  #Merge information with the association output 
   assoc <- merge(assoc, snp.frame)
   
+  #Create output frame
   out.frame <- data.frame(
     MARKERNAME=assoc$snp.id,
     EA=assoc$alt.allele,
